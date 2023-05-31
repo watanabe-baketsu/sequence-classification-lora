51706f242e532e95365fa013aaeceb95172d99e9
r-wata <109280369+watanabe-baketsu@users.noreply.github.com>
2023-05-31 23:56:41 +0900

    :+1: Add data visualization function

--- src/simple-classify.py
+++ src/simple-classify.py
@@ -2,9 +2,13 @@ import json
 from argparse import ArgumentParser
 
 import numpy as np
+import pandas as pd
 import torch
 from datasets import Dataset, DatasetDict
+from sklearn.preprocessing import MinMaxScaler
 from transformers import AutoModel, AutoTokenizer
+from matplotlib import pyplot as plt
+from umap import UMAP
 
 
 def read_dataset(file_path: str) -> DatasetDict:
@@ -41,15 +45,35 @@ def extract_hidden_states(batch):
     inputs = {k: v.to(args.device) for k, v in batch.items() if k in tokenizer.model_input_names}
     with torch.no_grad():
         last_hidden_state = model(**inputs).last_hidden_state
-    return {"hidden_state": last_hidden_state[:0].cpu().numpy()}
+    return {"hidden_state": last_hidden_state[:, 0].cpu().numpy()}
 
 
 def visualize_dataset_features(dataset: DatasetDict):
     X_train = np.array(dataset["training"]["hidden_state"])
-    X_valid = np.array(dataset["validation"]["hidden_state"])
     y_train = np.array(dataset["training"]["label"])
-    y_valid = np.array(dataset["validation"]["label"])
-    X_train.shape, X_valid.shape, y_train.shape, y_valid.shape
+
+    # Scale the features
+    X_scaled = MinMaxScaler().fit_transform(X_train)
+    # initialize umap and fit the scaled features
+    mapper = UMAP(n_components=2, metric="cosine").fit(X_scaled)
+    # create the embeddings
+    df_emb = pd.DataFrame(mapper.embedding_, columns=["X", "Y"])
+    df_emb["label"] = y_train
+    df_emb.head()
+
+    fig, axes = plt.subplots(2, 1, figsize=(7, 5))
+    axes = axes.flatten()
+    cmaps = ["Blues", "Reds"]
+    labels = ["not-phish", "phish"]
+
+    for i, (label, cmap) in enumerate(zip(labels, cmaps)):
+        df_emb_sub = df_emb.query(f"label == {i}")
+        axes[i].hexbin(df_emb_sub["X"], df_emb_sub["Y"], cmap=cmap, gridsize=20, linewidths=(0,))
+        axes[i].set_title(label)
+        axes[i].set_xticks([]), axes[i].set_yticks([])
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -65,8 +89,8 @@ if __name__ == "__main__":
 
     # Load dataset
     dataset = read_dataset(args.dataset_path)
-    training_dataset = dataset["training"].shuffle(seed=42).select(range(500))
-    validation_dataset = dataset["validation"].shuffle().select(range(100))
+    training_dataset = dataset["training"].shuffle(seed=42).select(range(200))
+    validation_dataset = dataset["validation"].shuffle().select(range(50))
     print(f"Training dataset count: {len(training_dataset)}")
     print(f"Validation dataset count: {len(validation_dataset)}")
     dataset = DatasetDict({
@@ -75,12 +99,13 @@ if __name__ == "__main__":
     })
 
     # Tokenize the texts
-    dataset = dataset.map(tokenize, batched=True, remove_columns=["text"])
+    dataset = dataset.map(tokenize, batched=True, batch_size=None)
     # Convert some columns to torch tensors
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
     # Extract hidden states
     dataset = dataset.map(extract_hidden_states, batched=True)
 
+    # Visualize the dataset features
     visualize_dataset_features(dataset)
 
 
